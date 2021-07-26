da88e7de9f16fef746f354c95e4972b4ee15b56b
VCarlosSoares <vitorcarlos2014soares@gmail.com>
2021-07-26 07:30:45 -0300

    Aulas introdutórias e de boas práticas

---
+++ 03-VariaveisMneumonicas.py
@@ -0,0 +1,19 @@
+### Maneiras de se nomear variáveis ###
+### Pior Maneira de fazer: ###
+
+a1e3k2k4j5m2 = 35.0
+b4q6j4l0d4j9 = 12.5
+v4q6e8r9a0s9 = a1e3k2k4j5m2 * b4q6j4l0d4j9
+print(v4q6e8r9a0s9)
+
+### Maneira Ok de fazer: ###
+a = 35.0
+b = 12.5
+c = a * b
+print(c)
+
+### Melhor maneira de fazer: ###
+horas = 35.0
+taxa = 12.5
+pagamento = horas * taxa
+print(pagamento)
